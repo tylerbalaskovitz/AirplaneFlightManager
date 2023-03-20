f6f2e842a19a0ecb8039a3c6cef7f01a003d6f76
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2023-03-20 14:40:58 -0400

    Test

--- src/main/java/services/FlightService.java
+++ src/main/java/services/FlightService.java
@@ -2,6 +2,12 @@ package services;
 
 public interface FlightService {
 
+	
+	/*
+	 * Keep flight parameters adjusted at the Interface level first as a way to keep things organized neatly
+	 * 
+	 */
+	
 	public void addFlight(String flightId, String airlines, String source, String destination, Double fare, Integer seatCount);
 	
 	public String generateFlightID(String flightId);
