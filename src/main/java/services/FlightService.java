796a6ef588e977baabd416f2135b200a0506c3d7
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2023-03-20 14:34:37 -0400

    Begun to set values and generate methods to taken in parameters that allows for the flight information to be put within a method

--- src/main/java/services/FlightService.java
+++ src/main/java/services/FlightService.java
@@ -2,7 +2,9 @@ package services;
 
 public interface FlightService {
 
-	public void addFlight();
+	public void addFlight(String flightId, String airlines, String source, String destination, Double fare, Integer seatCount);
+	
+	public String generateFlightID(String flightId);
 	
 	public void searchFlight();
 		
--- src/main/java/services/FlightServiceImpl.java
+++ src/main/java/services/FlightServiceImpl.java
@@ -1,6 +1,6 @@
 package services;
 
-import java.time.LocalDate;
+import java.util.ArrayList;
 
 import org.springframework.stereotype.Service;
 
@@ -9,7 +9,11 @@ import com.tbonegames.flights.Flight;
 @Service
 public class FlightServiceImpl extends Flight implements FlightService{
 
-	/*Adding a flight must have the user's Details 	private String flightId;
+	ArrayList <Flight> flightList = new ArrayList<>();
+	Flight flight;
+	
+	/*Adding a flight must have the user's Details 	
+	private String flightId;
 	private String airlines;
 	private String source;
 	private String destination;
@@ -17,14 +21,48 @@ public class FlightServiceImpl extends Flight implements FlightService{
 	private LocalDate journeyDate;
 	private Integer seatCount;
 	*/
-	public void addFlight() {
+	public void addFlight(String flightId) {
+		
 		
 	}
 	
+	@Override
+	public String generateFlightID(String flightId) {
+		String currentFlight = flightId;
+		if (flightId == null) {
+		this.setFlightId("1001");
+		} else {
+			currentFlight = flightId;
+		}
+		return currentFlight;
+		
+
+		
+	}
 	
 	
 	public void searchFlight() {
 		
 	}
+
+	@Override
+	public void addFlight(String flightId, String airlines, String source, String destination, Double fare, Integer seatCount) {
+
+		flight.setFlightId(flightId);
+		
+		flight.setAirlines(airlines);
+		
+		flight.setSource(source);
+		
+		flight.setDestination(destination);
+		
+		flight.setFare(fare);
+		
+		flight.setSeatCount(seatCount);
+		
+		flightList.add(flight);
+		
+	}
+
 	
 }
