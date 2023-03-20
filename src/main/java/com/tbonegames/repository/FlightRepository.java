5a4cd2f259efe77f790943efb9f634340a577118
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2023-03-20 15:23:41 -0400

    Added generic methods to the project and have begun to use the Map collection to gather values from a database which can then be used as options to search through and find information

--- src/main/java/com/tbonegames/repository/FlightRepository.java
+++ src/main/java/com/tbonegames/repository/FlightRepository.java
@@ -1,5 +1,30 @@
 package com.tbonegames.repository;
 
-public interface FlightRepository {
+/*
+ * implements the rquired repository using the map collection
+ *  Repositories are used to indicate that the class provides the mechanism for storage,
+ *  retrieval, search, update and delete operation on objects
+ *  
+ *  I am using Generics here, it’s a powerful technology to provide loosely coupled contract for the applications to implement
+ */
 
+/*Adding a flight must have the user's details. Which can also be used when searching for information.	
+private String flightId;
+private String airlines;
+private String source;
+private String destination;
+private Double fare;
+private LocalDate journeyDate;
+private Integer seatCount;
+*/
+
+public interface FlightRepository <T> {
+
+	public void store(T t);
+
+	public T retrieve(int id);
+
+	public T search(String name);
+
+	public T delete(int id);
 }
--- src/main/java/com/tbonegames/repository/FlightRepositoryImpl.java
+++ src/main/java/com/tbonegames/repository/FlightRepositoryImpl.java
@@ -1,8 +1,61 @@
 package com.tbonegames.repository;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class FlightRepositoryImpl implements FlightRepository{
+import com.tbonegames.flights.Flight;
 
+
+/*
+ * implements the rquired repository using the map collection
+ *  Repositories are used to indicate that the class provides the mechanism for storage,
+ *  retrieval, search, update and delete operation on objects
+ */
+
+@Repository
+public class FlightRepositoryImpl extends Flight implements FlightRepository{
+
+	private Map<Integer, Flight> repository;
+	
+	public FlightRepositoryImpl() {
+		this.repository = new HashMap<>();
+	}
+
+	@Override
+	public void store(Flight flight) {
+		repository.put(flight.getFlightId(), flight);
+	}
+
+	@Override
+	public Flight retrieve(int id) {
+		
+		return repository.get(id);
+	}
+
+	@Override
+	public Flight search(String name) {
+		//returns a collection of the values conatined in the map via the .values() method of the map collection
+		Collection<Flight> flightCollection = repository.values();
+		for (Flight flights: flightCollection) {
+			if (flights.getDestination().equalsIgnoreCase(name)) {
+				return flights;
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public Object delete(int id) {
+		
+		return null;
+	}
+	
+	
+	
+	 
+	
+	
 }
