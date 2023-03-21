608f77b820c6a18f7fae30cbae27fe09bb1c06cc
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2023-03-21 10:06:52 -0400

    Created a serial ID creation tool that allows for a unique serial ID to be generated

--- src/main/java/com/tbonegames/flights/Flight.java
+++ src/main/java/com/tbonegames/flights/Flight.java
@@ -1,16 +1,34 @@
 package com.tbonegames.flights;
 
 import java.time.LocalDate;
+import java.util.Random;
 
 public class Flight {
+	Random rand = new Random();
+	public Flight() {
+		serialId = generateSerialId();
+	}
+	
+	private int generateSerialId(){
+		
+		int id = 0;
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < 5; i++) {
+		id = rand.nextInt(10000);
+		while(id < 1000) {
+			id = rand.nextInt(10000);
+		}
+		sb.append(id);
+		
+		}
+		
+		String value = sb.toString();
+		id = Integer.parseInt(value);
+		return id;
+		
+	}
 
 	private Integer serialId;
-	public Integer getSerialId() {
-		return serialId;
-	}
-	public void setSerialId(Integer serialId) {
-		this.serialId = serialId;
-	}
 	private String flightId;
 	private String airlines;
 	private String source;
@@ -60,6 +78,12 @@ public class Flight {
 	}
 	public void setSeatCount(Integer seatCount) {
 		this.seatCount = seatCount;
+	}
+	public Integer getSerialId() {
+		return serialId;
+	}
+	public void setSerialId(Integer serialId) {
+		this.serialId = serialId;
 	}
 	
 }
