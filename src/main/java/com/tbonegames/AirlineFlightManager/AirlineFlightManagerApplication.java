c5544cf983e583219773d5422a1fa1e4848bc531
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2023-03-23 13:44:24 -0400

    Begun to add logging using AspectJ logging, by setting the PointCut above the method. Advice still needs to be put on each method

--- src/main/java/com/tbonegames/AirlineFlightManager/AirlineFlightManagerApplication.java
+++ src/main/java/com/tbonegames/AirlineFlightManager/AirlineFlightManagerApplication.java
@@ -1,10 +1,8 @@
 package com.tbonegames.AirlineFlightManager;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.tbonegames.repository.FlightRepositoryImpl;
--- src/main/java/com/tbonegames/flights/LoggingAspect.java
+++ src/main/java/com/tbonegames/flights/LoggingAspect.java
@@ -1,7 +1,20 @@
 package com.tbonegames.flights;
 
+
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class LoggingAspect {
 	
 	//logs the details for whenever a new flight is booked into a log file
-
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	//make sure you have a closing parenthesis on the right side after the quotation mark for the @Pointcut
+	@Pointcut("within(@com.tbonegames.repository.FlightRepositoryImpl *)")
+	public void afterAddingFlight(Object returnValue) throws Throwable{
+		logger.info("The flight information {} was propperly logged", returnValue);
+	}
+	
 }
