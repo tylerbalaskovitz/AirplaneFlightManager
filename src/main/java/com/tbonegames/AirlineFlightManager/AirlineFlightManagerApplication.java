2b2dfda875a9ade655e9afaf5f22797f6e16a6b6
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2023-03-24 15:20:42 -0400

    Was able to throw a different Exception while running the program

--- src/main/java/com/tbonegames/AirlineFlightManager/AirlineFlightManagerApplication.java
+++ src/main/java/com/tbonegames/AirlineFlightManager/AirlineFlightManagerApplication.java
@@ -1,5 +1,6 @@
 package com.tbonegames.AirlineFlightManager;
 
+import org.springframework.beans.BeansException;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,56 +13,52 @@ import com.tbonegames.services.FlightServiceImpl;
 @SpringBootApplication
 public class AirlineFlightManagerApplication{
 
-	//@Autowired
-//	private FlightServiceImpl flightService;
 	
 	public static void main(String[] args) {
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-		//ApplicationContext context = new ApplicationContext();
-		context.scan("com.tbonegames");
-		context.register(AppConfig.class);
-		context.refresh();
-		SpringApplication.run(AirlineFlightManagerApplication.class, args);
+		try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext()) {
+			//ApplicationContext context = new ApplicationContext();
+			context.scan("com.tbonegames");
+			context.register(AppConfig.class);
+			context.refresh();
+			SpringApplication.run(AirlineFlightManagerApplication.class, args);
 
-		FlightServiceImpl fsi = context.getBean(FlightServiceImpl.class);
-		fsi.addFlight("1004", "American Airlines", "Chicago", "Miami", 125.00, 3);
-		fsi.addFlight("2072", "United Airlines", "Atlanta", "Los Angeles", 255.00, 8);
-		fsi.addFlight(null, "All Nippon Airways", "Tokyo", "Chicago", 1400.00, 1);
-		
-		FlightRepositoryImpl fri = context.getBean(FlightRepositoryImpl.class);
-		System.out.println(" ");
-		System.out.println("=========Adding Flights=========");
-		for (int i = 0; i < fri.getRepository().size(); i++) {
-			System.out.println(fri.retrieve(i).toString());
+			FlightServiceImpl fsi = context.getBean(FlightServiceImpl.class);
+			fsi.addFlight("1004", "American Airlines", "Chicago", "Miami", 125.00, 3);
+			fsi.addFlight("2072", "United Airlines", "Atlanta", "Los Angeles", 255.00, 8);
+			fsi.addFlight(null, "All Nippon Airways", "Tokyo", "Chicago", 1400.00, 1);
 			
-		}
-		
-		System.out.println(" ");
-		System.out.println("-----------AFTER SEARCH-----------");
-		System.out.println(fri.search("TOkyo").toString());
-		System.out.println(fri.search("miami").toString());
-		System.out.println(" ");
-		
-		System.out.println("-----------Record Deletion-----------");
-		fri.delete(2072);
-		fri.delete(1004);
-		System.out.println(" ");
-		System.out.println("-----------AFTER DELETION-----------");
-
-		//since this is a map, it uses two objects as the 
-		for (int i = 0; i < Integer.MAX_VALUE; i++) {
-			if (fri.retrieve(i) != null) {
+			FlightRepositoryImpl fri = context.getBean(FlightRepositoryImpl.class);
+			System.out.println(" ");
+			System.out.println("=========Adding Flights=========");
+			for (int i = 0; i < fri.getRepository().size(); i++) {
 				System.out.println(fri.retrieve(i).toString());
+				
 			}
+			
+			System.out.println(" ");
+			System.out.println("-----------AFTER SEARCH-----------");
+			System.out.println(fri.search("TOkyo").toString());
+			System.out.println(fri.search("miami").toString());
+			System.out.println(" ");
+			
+			System.out.println("-----------Record Deletion-----------");
+			fri.delete(2072);
+			fri.delete(1004);
+			System.out.println(" ");
+			System.out.println("-----------AFTER DELETION-----------");
+
+			//since this is a map, it uses two objects as the 
+			for (int i = 0; i < Integer.MAX_VALUE; i++) {
+				if (fri.retrieve(i) != null) {
+					System.out.println(fri.retrieve(i).toString());
+				}
+			}
+		} catch (BeansException | IllegalStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
 		
 
 	}
-	/*
-	@Override
-    public void run(String... args) throws Exception {
-        flightService.addFlight("1004", "American Airlines", "Chicago", "Miami", 125.00, 3);
-    }
-	*/
 }
--- src/main/java/com/tbonegames/flights/AppConfig.java
+++ src/main/java/com/tbonegames/flights/AppConfig.java
@@ -1,6 +1,5 @@
 package com.tbonegames.flights;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
@@ -13,7 +12,6 @@ public class AppConfig {
 	FlightRepositoryImpl fri;
 	
 
-	
 	public LoggingAspect loggingAspect() {
 		return new LoggingAspect();
 	}
